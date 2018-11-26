807360d7c45dd38bcb33aee8eecf3bdbebd30e8b
Le Koj <mitchkojelegance@gmail.com>
2018-11-26 13:16:12 -0800

    DCP_3 code version 2
    2nd version of program by division

--- DCP_3.py
+++ DCP_3.py
@@ -30,5 +30,22 @@ def mutate(arr):
         _new_container.append(value)    #-- add calculated value to new array
 
     return _new_container
+    
+def mutate_v2(arr):
+	_new_container = []     #--- an empty list to hold new array
+	_sub_multiple = 1
+	
+	# find total multiple of set
+	for i in arr:
+		_sub_multiple *= i
+		
+	# create new array
+	# divide total multiple by current index value
+	# add quotient to new array
+	for i in arr:
+		quotient = _sub_multiple/i
+		_new_container.append(quotient)
+		
+	return _new_container
                 
 
