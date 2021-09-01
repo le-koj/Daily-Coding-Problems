b50d3d156447e2a890851c271009e7b54bd4e161
Le Koj <mitchkojelegance@gmail.com>
2021-09-01 02:16:12 -0700

    unittest run on rotate_array method

--- rotate_array.py
+++ rotate_array.py
@@ -1,3 +1,5 @@
+import unittest
+
 def rotate_array(arr: list, k: int):
     """
     Given an array on N length, rotate the array rightwards by K rotations, i.e 
@@ -5,3 +7,17 @@ def rotate_array(arr: list, k: int):
     """
 
     return arr[-k::] + arr[:(len(arr) - k)]
+
+
+class TestRotateArray(unittest.TestCase):
+
+    def test_rotate_array(self):
+        self.assertEqual(rotate_array([1,2,3,4,5,6,7], 2), [6,7,1,2,3,4,5], "should be [6,7,1,2,3,4,5]")
+        self.assertEqual(rotate_array([1,2,3,4,5,6,7], 3), [5,6,7,1,2,3,4], "should be [5,6,7,1,2,3,4]")
+        self.assertEqual(rotate_array(['john', 'jacob', 'jingle', 'hummer', 'smith'], 1), ['smith', 'john', 'jacob', 'jingle', 'hummer'], "should be ['smith', 'john', 'jacob', 'jingle', 'hummer']")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
